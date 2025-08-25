5652c132f510eb81485655ebd9387ac6b0125e27
Amr Khaled <redwolf4325@gmail.com>
2025-08-25 03:56:08 +0300

    Used DeviceApps and it works better and added a condition to filter the internal apps

--- lib/screens/apps.dart
+++ lib/screens/apps.dart
@@ -4,7 +4,6 @@ import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focus_launcher/store/apps_pod.dart';
-import 'package:installed_apps/index.dart';
 
 class AppsScreen extends ConsumerStatefulWidget {
   const AppsScreen({super.key});
@@ -14,48 +13,24 @@ class AppsScreen extends ConsumerStatefulWidget {
 }
 
 class _AppsScreenState extends ConsumerState<AppsScreen> {
-  void getApps() async {
-    List<AppInfo> apps = [];
-    for (BuiltWith built in BuiltWith.values) {
-      List<AppInfo> _apps =
-          await InstalledApps.getInstalledApps(false, false, '', built);
-      apps.addAll(_apps);
-    }
-    List<AppInfo> _apps = [];
-    print(apps.length);
-    for (AppInfo app in apps.toSet().toList()) {
-      bool names = app.name.startsWith('com.');
-      bool packages = app.packageName.startsWith('com.android');
-      bool packagesGoogle =
-          app.packageName.startsWith('com.google.android.apps');
-      if (!packages && !names && packagesGoogle) {
-        print("${app.packageName} $names");
-        _apps.add(app);
-      }
-    }
-    print(_apps.length);
-    ref.watch(appsPod).updateList(_apps.toSet().toList());
-    print(_apps.length);
-  }
-
-  List<Widget> viewApps(List<AppInfo> apps) {
+  List<Widget> viewApps(List<Application> apps) {
     return List.generate(apps.length, (i) {
       return ListTile(
         key: Key('$i'),
-        title: Text(apps[i].name, style: const TextStyle(color: Colors.white)),
-        onTap: () async => await InstalledApps.startApp(apps[i].packageName),
+        title:
+            Text(apps[i].appName, style: const TextStyle(color: Colors.white)),
+        onTap: () async => await DeviceApps.openApp(apps[i].packageName),
         onLongPress: () =>
             setState(() => longPressedAppPackage = apps[i].packageName),
       );
     });
   }
 
-  List<Application> newlyInstalledApps = [];
-  List<Widget> newlyInstalledAppsViews = [];
+  List<Application> newlyDeviceApps = [];
+  List<Widget> newlyDeviceAppsViews = [];
   @override
   void initState() {
     super.initState();
-    getApps();
   }
 
   String longPressedAppPackage = '';
